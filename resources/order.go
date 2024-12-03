55dac4460cad65d4356db1749d6cd65f7d36c566
vizvasrj None <vizvasrj@gmail.com>
2024-12-03 14:34:51 +0000

    Refactor Order.Create method to use OrderCreateRequest and OrderCreateResponse structs
    - Updated Order.Create method to accept OrderCreateRequest struct instead of map[string]interface{}.
    - Modified Order.Create method to return OrderCreateResponse struct instead of map[string]interface{}.
    - Added JSON marshaling and unmarshaling to convert between structs and JSON data.
    - Improved type safety and readability by using defined structs for request and response data.
    
    Refactor TestOrderCreate to use resources.OrderCreateRequest instead of map[string]interface{}
    
    - Updated TestOrderCreate to use resources.OrderCreateRequest struct for request parameters.
    - Replaced map[string]interface{} with resources.OrderCreateRequest in the test.
    - Ensured the test setup and assertions remain consistent with the new struct.

--- resources/order.go
+++ resources/order.go
@@ -1,13 +1,14 @@
 package resources
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/razorpay/razorpay-go/constants"
 	"github.com/razorpay/razorpay-go/requests"
 )
 
-//Order ...
+// Order ...
 type Order struct {
 	Request *requests.Request
 }
@@ -25,9 +26,24 @@ func (order *Order) Fetch(orderID string, queryParams map[string]interface{}, ex
 }
 
 // Create creates a new order for the given data
-func (order *Order) Create(data map[string]interface{}, extraHeaders map[string]string) (map[string]interface{}, error) {
+func (order *Order) Create(inputData OrderCreateRequest, extraHeaders map[string]string) (OrderCreateResponse, error) {
 	url := fmt.Sprintf("/%s%s", constants.VERSION_V1, constants.ORDER_URL)
-	return order.Request.Post(url, data, extraHeaders)
+	data, err := order.Request.Post(url, inputData, extraHeaders)
+	var orderEntity OrderCreateResponse
+	if err != nil {
+		return orderEntity, err
+	}
+
+	bytesData, err := json.Marshal(data)
+	if err != nil {
+		return orderEntity, err
+	}
+
+	err = json.Unmarshal(bytesData, &orderEntity)
+	if err != nil {
+		return orderEntity, err
+	}
+	return orderEntity, nil
 }
 
 // Update updates an order having the given orderID.
