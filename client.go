57013a9296d5e97cd70a165cbda636167eeb9a56
Chirag Chiranjib <chirag.chiranjib@razorpay.com>
2025-06-10 17:57:21 +0530

    fix: initialise headers for request

--- client.go
+++ client.go
@@ -44,9 +44,14 @@ type Client struct {
 func NewClient(key string, secret string) *Client {
 	auth := requests.Auth{Key: key, Secret: secret}
 	httpClient := &http.Client{Timeout: requests.TIMEOUT * time.Second}
-	request := &requests.Request{Auth: auth, HTTPClient: httpClient,
-		Version: getVersion(), SDKName: getSDKName(),
-		BaseURL: constants.BASE_URL}
+	request := &requests.Request{
+		Auth:       auth,
+		HTTPClient: httpClient,
+		Version:    getVersion(),
+		SDKName:    getSDKName(),
+		BaseURL:    constants.BASE_URL,
+		Headers:    make(map[string]string),
+	}
 
 	account := resources.Account{Request: request}
 	addon := resources.Addon{Request: request}
